e3148a808d31520871cf3d7b5d4681dce29cf423
Nicolasalan <nicolasalangrotti03@gmail.com>
2024-04-01 23:45:42 -0300

    try matplotlib

--- src/robot_localization/robot_localization/localization.py
+++ src/robot_localization/robot_localization/localization.py
@@ -13,9 +13,17 @@ from math import exp, sqrt, pi
 from math import cos, sin
 import numpy as np
 
-from matplotlib import pyplot as plt
+try:
+    from matplotlib import pyplot as plt
+    PLOT = True
+except:
+    PLOT = False
 
-Inf = float('inf')
+MIN_RANGE = 1.50
+MAX_RANGE = 1.60
+
+MAP = [-1.4806214474165549, -1.5211909114391624]
+INI = -1.6513183381797376
 
 class Localization(Node):
 
@@ -44,10 +52,9 @@ class Localization(Node):
         self.ultimas_medidas = [0, 0]
         self.distancias = [0, 0]
 
-        self.estado_inicial = -1.999
-        self.mapa = [-1.66, -0.561, 0.569]
+        self.estado_inicial = INI # -1.999
+        self.mapa = MAP #  -1.4806214474165549 ou  -1.462728444945766, | -1.5211909114391624 # [-1.66, -0.561, 0.569]
         self.pose[0] = self.estado_inicial
-
         self.sigma_odometria = 0.2 # rad
         self.sigma_lidar = 0.175 # meters
         self.sigma_movimento = 0.002 # m
@@ -55,11 +62,12 @@ class Localization(Node):
         self.porta = 0
         self.controle = 0
 
-        self.x = np.linspace(-4.5, 4.5, 500) # cria um vetor x de 500 valores entre -4.5 e 4.5
-        self.y = np.zeros(500) # cria um vetor y de 500 valores zeros
-        self.y2 = np.zeros(500)
-        self.y3 = np.zeros(500)
-        self.fig, self.ax = plt.subplots()
+        if PLOT:
+            self.x = np.linspace(-4.5, 4.5, 500) # cria um vetor x de 500 valores entre -4.5 e 4.5
+            self.y = np.zeros(500) # cria um vetor y de 500 valores zeros
+            self.y2 = np.zeros(500)
+            self.y3 = np.zeros(500)
+            self.fig, self.ax = plt.subplots()
 
     def gaussian(self, x, mean, sigma):
         return (1 / (sigma*sqrt(2*pi))) * exp(-((x-mean)**2) / (2*sigma**2))
@@ -86,40 +94,36 @@ class Localization(Node):
 
     def joint_callback(self, msg):
         self.joint = msg.position
-        #self.get_logger().info(str(self.joint))
 
     def cmd_vel_callback(self, msg):
         self.cmd = msg.linear.x
-        #self.get_logger().info(str(self.joint))
 
     def listener_callback_laser(self, msg):
         self.laser = msg.ranges
-        #self.get_logger().info(str(self.laser))
 
     def listener_callback_odom(self, msg):
         self.position = msg.pose.pose
-        #self.get_logger().info(str(self.pose))
 
     def navigation_start(self):
         self.get_logger().info("Inicialize seu código aqui!")
 
     def navigation_update(self):
-      #self.get_logger().info("Aqui acontece o loop principal do seu código!")
       leitura = self.laser
       cont = 0
 
-      self.update() # atualiza a nova pose do robô
+      self.update()
 
       print("Sigma_movimento: ", self.sigma_movimento)
       print("Pose: ", self.pose[0])
 
-      if cont % 4 == 0: # a cada 4 passos, plotar em preto “b” a gaussiana da posição do robô em
-        for i in range(len(self.x)):
-            self.y[i] = self.gaussian(self.x[i], self.pose[0], self.sigma_movimento)
-        self.ax.clear()
-        self.ax.set_ylim([0, 4])
-        self.ax.plot(self.x, self.y, color="b")
-        plt.pause(0.1)
+      if PLOT:
+        if cont % 4 == 0: # a cada 4 passos, plotar em preto “b” a gaussiana da posição do robô em
+            for i in range(len(self.x)):
+                self.y[i] = self.gaussian(self.x[i], self.pose[0], self.sigma_movimento)
+            self.ax.clear()
+            self.ax.set_ylim([0, 4])
+            self.ax.plot(self.x, self.y, color="b")
+            plt.pause(0.1)
 
       if self.cmd > 0:
             self.controle = 1
@@ -127,25 +131,28 @@ class Localization(Node):
       if len(leitura) == 0:
           pass
       else:
+        print("Leitura: ", leitura[72], leitura[108])
         if self.controle == 1:
             self.sigma_movimento = self.sigma_movimento + 0.002
-        if (leitura[72] >= 1.50 and leitura[72] <= 1.60) and (leitura[108] >= 1.50 and leitura[108] <= 1.60):
+        if (leitura[72] >= MIN_RANGE and leitura[72] <= MAX_RANGE) and (leitura[108] >= MIN_RANGE and leitura[108] <= MAX_RANGE):
             media_nova = (self.mapa[self.porta] * self.sigma_movimento + self.pose[0]*self.sigma_lidar) / (self.sigma_movimento+self.sigma_lidar)
             sigma_novo = 1 / (1/self.sigma_movimento + 1/self.sigma_lidar)
 
             self.pose[0] = media_nova # a nova posição x do robô
 
-            for i in range(len(self.x)):
-                self.y2[i] = self.gaussian(self.x[i], self.mapa[self.porta], self.sigma_lidar)
-                self.ax.plot(self.x, self.y2, color="r")
-                plt.pause(0.1)
+            if PLOT:
+                for i in range(len(self.x)):
+                    self.y2[i] = self.gaussian(self.x[i], self.mapa[self.porta], self.sigma_lidar)
+                    self.ax.plot(self.x, self.y2, color="r")
+                    plt.pause(0.1)
 
             sigma_movimento = sigma_novo
 
-            for i in range(len(self.x)):
-                self.y3[i] = self.gaussian(self.x[i], media_nova, sigma_novo)
-                self.ax.plot(self.x, self.y3, color="g")
-                plt.pause(0.1)
+            if PLOT:
+                for i in range(len(self.x)):
+                    self.y3[i] = self.gaussian(self.x[i], media_nova, sigma_novo)
+                    self.ax.plot(self.x, self.y3, color="g")
+                    plt.pause(0.1)
 
             if self.porta == 0:
                 self.porta = 1 # altera para a próxima porta 0 → 1 ; 1 → 2
